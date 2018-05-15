6550e0a120efa91bff87ccfb628ed613549fce70
unknown <juandagonxalezh@gmail.com>
2018-05-15 11:44:46 -0500

    connected the style files with prepros and added the images designed for the index

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database = 'mongodb://localhost:27017/databasename', //change the name of the database
+    database = 'mongodb://localhost:27017/business', //change the name of the database
     secret= 'yoursecret'
 }
